4a88bf5755501392d510447a5165365e55629b43
EamonZhong <qddxzym@163.com>
2017-10-01 22:12:00 +0800

    vue-router.v1

---
+++ vue__router/vueclitest/src/components/Hi1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+     <h2>{{msg}}</h2> 
+  </div>
+</template>
+<script>
+    export default {
+        name: 'hi',
+        data() {
+            return {
+                msg: 'I am Hi1 page'
+            }
+        }   
+    }
+</script>
+<style scoped>
+
+</style>
---
+++ vue__router/vueclitest/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Hi from '@/components/Hi'
+import Hi1 from '@/components/Hi1'
+import Hi2 from '@/components/Hi2'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path:'/Hi',
+      name:'Hi',
+      component:Hi,
+      children:[
+        {path:'/',component:Hi},
+        {path:'hi1',component:Hi1},
+        {path:'hi2',component:Hi2}
+
+      ]
+    }
+  ]
+})
