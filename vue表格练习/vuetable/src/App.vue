cb9c15fc027dac2c869021fee8d0bb61edf36ca6
EamonZhong <qddxzym@163.com>
2017-10-25 22:12:22 +0800

    vue表格练习

---
+++ vue表格练习/vuetable/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Tablede></Tablede>
+  </div>
+</template>
+
+<script>
+import Tablede from './components/Table'
+
+export default {
+  name: 'app',
+  components: {
+    Tablede
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 30px auto;
+  width: 80%;
+  padding: 20px;
+  height: 650px;
+  border: 1px solid #000;
+}
+</style>
