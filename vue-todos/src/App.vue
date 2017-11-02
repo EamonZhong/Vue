c0d7f5135dd944c56886704251a80d98b20f7715
EamonZhong <qddxzym@163.com>
2017-11-02 22:13:01 +0800

    todos-way2

---
+++ vue-todos/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <ul class="todos">
+      <li v-for="(todo, index) in todos" :key="index" :id="index"> 
+        <label>{{todo.value}}</label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+     title: 'vue-todos',
+     todos:  [
+       {value: '阅读一本关于前端开发的书', done: false},
+       {value: '补充示例代码', done: true},
+       {value: '写心得' , done: false}
+     ],
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+@import './assets/site.less';
+@import './assets/todo.less';
+
+</style>
